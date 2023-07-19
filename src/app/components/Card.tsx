a6b73722719b3bbc0791e8ddfb202cb6514b9ca3
JC <jcsacra4@gmail.com>
2023-07-19 14:55:24 +0800

    Form Validation is still ongoing

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function Card({ children, title, link }: Props) {
   return (
     <div className="border border-black rounded-md p-6 flex flex-col">
-      <a href={link} className="flex flex-row items-center font-bold">{title}<Image height={18}  width={18} src={"/icons/link.svg"} alt={"Link logo"}/></a>
+      <a href={link} className="flex flex-row items-center gap-1 font-bold">{title}<Image height={18}  width={18} src={"/icons/upper-right-arrow.svg"} alt={"Link logo"}/></a>
       <p>{children}</p>
     </div>
   );
--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Image from "next/image";
 import emailjs from "@emailjs/browser";
 import Button from "./Button";
@@ -7,15 +7,24 @@ import Button from "./Button";
 type Props = {};
 
 export default function Form({}: Props) {
+  const [isValidName, setIsValidName] = useState(false);
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [isValidMessage, setIsValidMessage] = useState(false);
+  const [isFormValid, setIsFormValid] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+
   const form = useRef<HTMLFormElement | null>(null);
+  const statusMessageRef = useRef<HTMLSpanElement>(null);
   const username = useRef<HTMLSpanElement>(null);
   const email = useRef<HTMLSpanElement>(null);
   const message = useRef<HTMLSpanElement>(null);
 
   const sendEmail = (e: React.SyntheticEvent) => {
     e.preventDefault();
-
-    if (form.current) {
+    const isFormValid = isValidName && isValidEmail && isValidMessage;
+    statusMessageRef.current?.classList.remove('hidden')
+    setIsFormValid(isFormValid)
+    if (form.current && isFormValid) {
       emailjs
         .sendForm(
           "service_yr6eqbn",
@@ -33,13 +42,19 @@ export default function Form({}: Props) {
             console.log(error.text);
           }
         );
+    } else {
+      console.log("Invalid Form Input");
     }
+  };
+
+  const focusHandler = () => {
+    setIsFocused(true);
   };
 
   const blurHandler = (
     event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>,
     ref: React.RefObject<HTMLSpanElement>
-  ) => {
+  ) => {  
     event.target.value || ref.current?.classList.remove("hidden");
   };
 
@@ -51,44 +66,62 @@ export default function Form({}: Props) {
       ? ref.current?.classList.add("hidden")
       : ref.current?.classList.remove("hidden");
 
+    switch (event.target.name) {
+      case "user_name":
+        setIsValidName(validateString(event));
+        break;
+      case "user_email":
+        setIsValidEmail(validateEmail(event));
+        break;
+      case "message":
+        setIsValidMessage(validateString(event));
+        break;
+    }
     event.target.type === "email"
       ? validateEmail(event)
       : validateString(event);
   };
   const validateEmail = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ): void => {
-    if (/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(e.target.value)) {
-      console.log("valid");
-    } else {
-      console.log("invalid");
-    }
-  };
+  ): boolean =>
+    /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(e.target.value);
+
   const validateString = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ): void => {
-    e.target.value ? console.log("valid") : console.log("invalid");
-  };
+  ): boolean => Boolean(e.target.value);
+
   return (
     <>
-      <form ref={form} className="flex flex-col gap-y-2 ">
+      <form ref={form} className="flex flex-col gap-y-2  mb-[470px]">
         <section className="formSection h-12 ">
           <label>
             <span ref={username} className="formSpan">
               Name
             </span>
-            <Image
-              src={"/icons/check-circle.svg"}
-              alt={"Correct Input"}
-              width={18}
-              height={18}
-              className="absolute bottom-1 right-1"
-            />
+            {isFocused &&
+              (isValidName ? (
+                <Image
+                  src={"/icons/check-circle.svg"}
+                  alt={"Correct Input"}
+                  width={18}
+                  height={18}
+                  className="absolute bottom-1 right-1 z-10"
+                />
+              ) : (
+                <Image
+                  src={"/icons/delete-circle.svg"}
+                  alt={"Correct Input"}
+                  width={18}
+                  height={18}
+                  className="absolute bottom-1 right-1 z-10"
+                />
+              ))}
 
             <input
               type="text"
               name="user_name"
               className="formInputs"
+              onFocus={(event) => focusHandler()}
               onBlur={(event) => blurHandler(event, username)}
               onChange={(event) => changeHandler(event, username)}
             />
@@ -99,17 +132,29 @@ export default function Form({}: Props) {
             <span ref={email} className="formSpan">
               E-mail
             </span>
-            <Image
-              src={"/icons/check-circle.svg"}
-              alt={"Correct Input"}
-              width={18}
-              height={18}
-              className="absolute bottom-1 right-1"
-            />
+            {isFocused &&
+              (isValidEmail ? (
+                <Image
+                  src={"/icons/check-circle.svg"}
+                  alt={"Correct Input"}
+                  width={18}
+                  height={18}
+                  className="absolute bottom-1 right-1 z-10"
+                />
+              ) : (
+                <Image
+                  src={"/icons/delete-circle.svg"}
+                  alt={"Correct Input"}
+                  width={18}
+                  height={18}
+                  className="absolute bottom-1 right-1 z-10"
+                />
+              ))}
             <input
               type="email"
               name="user_email"
               className="formInputs"
+              onFocus={(event) => focusHandler()}
               onBlur={(event) => blurHandler(event, email)}
               onChange={(event) => changeHandler(event, email)}
             />
@@ -123,16 +168,28 @@ export default function Form({}: Props) {
             >
               Message
             </span>
-            <Image
-              src={"/icons/check-circle.svg"}
-              alt={"Correct Input"}
-              width={18}
-              height={18}
-              className="absolute bottom-1 right-1 z-10"
-            />
+            {isFocused &&
+              (isValidMessage ? (
+                <Image
+                  src={"/icons/check-circle.svg"}
+                  alt={"Correct Input"}
+                  width={18}
+                  height={18}
+                  className="absolute bottom-1 right-1 z-10 "
+                />
+              ) : (
+                <Image
+                  src={"/icons/delete-circle.svg"}
+                  alt={"Correct Input"}
+                  width={18}
+                  height={18}
+                  className="absolute bottom-1 right-1 z-10"
+                />
+              ))}
             <textarea
               name="message"
               className="formInputs h-full w-full"
+              onFocus={(event) => focusHandler()}
               onBlur={(event) => blurHandler(event, message)}
               onChange={(event) => changeHandler(event, message)}
             />
@@ -140,11 +197,12 @@ export default function Form({}: Props) {
         </section>
         <Button
           type="submit"
-          additionalStyles={"mt-5 mb-[470px]"}
+          additionalStyles={"mt-5"}
           clickHandler={sendEmail}
         >
           Send
         </Button>
+        <span ref={statusMessageRef} className="hidden">{isFormValid?"Message Sent":"Invalid Input"}</span>
       </form>
     </>
   );
